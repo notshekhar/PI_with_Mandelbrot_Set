7e4b81fb9a5b1b7606169ee8baf35064ba5342ef
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-06-28 10:40:46 +0530

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,20 +1,42 @@
 from datetime import datetime
-
-digits = 9
+PI = 3.14159265359
+print(PI)
+digits = 7
 c = 0.25
 e = 1.0 / pow(100, digits-1)
 c = c+e
-z = 0
+z = 0.0
 iteration = 0
 d = datetime.now()
 while(z<2):
-  for i in range(100000):
+  for i in range(10000):
     if(z<2):
       z = pow(z, 2) + c
       iteration+=1
     else:
       break
 
+  j = "{}".format(iteration)
+  k = ""
+  diff = digits-len(j)
+  if(diff>0):
+    for i in range(diff):
+      if(i == 0):
+        k += "0."
+      else:
+        k += "0"
+    k += j
+  else: 
+    for i in range(len(j)):
+      if(i==0):
+        k += "{}.".format(j[i])
+      else: 
+        k += j[i]
+  print(k)
 
-print(datetime.now()-d)
-print(iteration)
+value = k
+print("")
+print("Total time to calculate: {}".format(datetime.now()-d))
+print("Original PI value: {}".format(PI))
+print("Calculated PI value: {}".format(value))
+print("Error: {}".format(PI - float(value)))
